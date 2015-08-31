0e015588ddaa8291cda56388769853cc0068b511
crsuperman34 <shawn.naquin@gmail.com>
2015-08-31 13:21:43 -0400

    uglify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -46,8 +46,7 @@ gulp.task('html', ['styles'], () => {
 
   return gulp.src('app/*.html')
     .pipe(assets)
-    .pipe('*.js')
-    //.pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.css', $.minifyCss({compatibility: '*'})))
     .pipe(assets.restore())
     .pipe($.useref())
